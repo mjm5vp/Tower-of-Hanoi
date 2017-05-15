9833e53f8e307913384ddfee13c58372101041ff
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-05-15 14:50:25 -0400

    beginning CSS

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+peg = $(".tower div")
+peg.on("click", clickedPeg)
+blockHeight = 20
+
+function clickedPeg(){
+  console.log("1: " + this.id)
+  tower.click1 = this.id
+  pegClick1 = this.id
+  $(this).toggleClass("clickedPeg")
+  peg.off("click")
+  peg.on("click", secondClickedPeg)
+  $(this).off("click")
+}
+
+function secondClickedPeg(){
+  pegClick2 = this.id
+  console.log("2: " + pegClick2)
+  tower.click2 = pegClick2
+  tower.move(tower[pegClick1],tower[pegClick2])
+
+
+  peg.removeClass("clickedPeg")
+  peg.off("click",secondClickedPeg)
+  peg.on("click", clickedPeg)
+
+  displayTower()
+}
+
 var tower = {
   peg1: [],
   peg2: [],
   peg3: [],
+
+  click1: "",
+  click2: "",
 
   blockSizeWorks(block1, block2){
     if (block1.size > block2.size) {
@@ -13,14 +44,25 @@ var tower = {
   },
 
   move(pegStart, pegEnd) {
-    console.log(pegStart)
+    //console.log("pegStart: " + pegStart)
+    //console.log("pegEnd: " + pegStart)
     var block1 = pegStart[pegStart.length - 1]
     var block2 = pegEnd[pegEnd.length - 1]
-    console.log("block1: " + block1)
-    console.log("block2: " + block2)
+    //console.log("block1: " + block1)
+    //console.log("block2: " + block2)
     if (block2 == undefined || this.blockSizeWorks(block1,block2) == true) {
       pegStart.pop()
       pegEnd.push(block1)
+    }
+    //tower.updateTower()
+  },
+
+  updateTower() {
+    for (var i = 0; i < this.peg1.length; i++) {
+
+      console.log(this.peg1[i].size)
+      this.peg1[i].css("bottom", bottomPosition)
+      $("#peg1").append(this.peg1[i])
     }
   }
 
@@ -31,19 +73,40 @@ var tower = {
 class Block{
   constructor(size){
     this.size = size
+//    this.position = 1
+  }
+
+  createBlock(i) {
+    var bottomPosition = ((24 * i) - 20) + "px"
+    var width = this.size * 10
+    var newBlock = $("<div></div>")
+    newBlock.addClass("block")
+    newBlock.css("width", width)
+    newBlock.css("bottom", bottomPosition)
+    $("#peg1").append(newBlock)
   }
 
 
 }
 
-for (var i = 4; i > 0; i--) {
+for (var i = 0; i < 5; i++) {
   var newBlock = new Block(i)
+  newBlock.createBlock(i)
   tower.peg1.push(newBlock)
 }
 
-tower.move(tower.peg1,tower.peg2)
-tower.move(tower.peg1,tower.peg2)
 
+
+
+
+//tower.move(tower.peg1,tower.peg2)
+//tower.move(tower.peg1,tower.peg2)
+
+
+function displayTower(){
 console.log("peg1 : " + tower.peg1)
 console.log("peg2 : " + tower.peg2)
 console.log("peg3 : " + tower.peg3)
+}
+
+//tower.updateTower()
